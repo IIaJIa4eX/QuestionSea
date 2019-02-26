84f572406cc41e75be081a21002c8f88e017b2e7
Student <Student@rubiconepro.ru>
2019-02-26 21:00:02 +0300

    Добавьте файлы проекта.

---
+++ QuestionSee/DB/DBConnection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuestionSee.DB
+{
+    public class DBConnection: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("Server=vtest.fvds.ru;Database=questionsee;Uid=newuser;Pwd=qwertypassword;CharSet=utf8;");
+        }
+    }
+}
---
+++ QuestionSee/DB/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuestionSee.DB
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int score { get; set; }
+
+        public bool login = false;
+
+    }
+}

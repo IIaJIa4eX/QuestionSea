e04ff42fff3b90a8734fb069a00be69c36b86e04
IIaJIa4eX <IIaJIa4eX@gmail.com>
2019-04-11 20:57:46 +0300

    Session AccInfo was added

--- QuestionSee/Controllers/HomeController.cs
+++ QuestionSee/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using QuestionSee.Models;
 using QuestionSee.DB;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace QuestionSee.Controllers
 {
@@ -18,7 +19,12 @@ namespace QuestionSee.Controllers
 
         public HomeController(Session ses)
         {
-            this.ses = ses;
+            this.ses = ses;            
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
             if (ses.users.Keys.Contains(HttpContext.Session.Id))
             {
                 CurrentUser = ses.users[HttpContext.Session.Id].current;
@@ -39,7 +45,7 @@ namespace QuestionSee.Controllers
 
             }
 
-            return View();
+            return RedirectToAction("RegistrationPage");
         }
 
 

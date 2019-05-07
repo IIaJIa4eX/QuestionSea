9d771e99e852e23945ea03174728014c2c707a3e
IIaJIa4eX <IIaJIa4eX@gmail.com>
2019-05-07 21:00:15 +0300

    Adding admin

--- QuestionSee/Components/FullQuestionInfo.cs
+++ QuestionSee/Components/FullQuestionInfo.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using QuestionSee.DB;
+using QuestionSee.Models;
+
 namespace QuestionSee.Components
 {
     public class FullQuestionInfoViewComponent : ViewComponent
     {
         DBConnection db;
+        User CurrentUser;
 
         public FullQuestionInfoViewComponent(DBConnection db)
         {
@@ -26,12 +30,21 @@ namespace QuestionSee.Components
             {
                 return View("empty");
             }
+
+            User u = new User();
+            u = CurrentUser;
+            
             string id = Request.Form["FullQuestionInfo"];
             int iid = int.Parse(id);
             var arr = db.Questions.Where(f => f.Id == iid).FirstOrDefault();
 
+            dynamic d = new ExpandoObject();
+            d.user = u;
+            d.question = arr;
 
-            return View(arr);
+            //{ user = u, question = arr };
+
+            return View(d);
         }
     }
 }
---
+++ QuestionSee/Models/BaseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace QuestionSee.Models
+{
+    public class BaseModel
+    {
+        public DB.User user { get; set; }
+    }
+}
